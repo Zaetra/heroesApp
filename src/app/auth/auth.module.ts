27122bf48331e3b8c111ccce84f3f4b05865eff5
Alexander Zarate <azaratech@gmail.com.com>
2024-09-08 19:56:07 -0500

    clase 184

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth.routing.module';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutPagesComponent,
+    LoginPagesComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+
+  ]
+})
+export class AuthModule { }
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchComponent } from './pages/search/search.component';
+
+
+@NgModule({
+  declarations: [
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
